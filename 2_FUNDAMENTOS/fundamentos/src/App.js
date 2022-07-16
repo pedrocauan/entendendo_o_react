3943240e525f0a3c807f9cf4f25c8b5b2ba8bd99
pedrocauan <mainyasuo92@gmail.com>
2022-07-16 03:41:23 -0300

    criando componentes e exportando default

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // ==== CODIGO AQUI  ====
+    <div className="App">
+      <h1>Fundamentos do Reeact</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,13 @@
+
+//Componente
+const FirstComponent = () => {
+    //O que ele vai retornar
+    return (
+        <div>
+            <h1>Primeiro componente</h1>
+        </div>
+    )
+}
+
+//faz com que ele possa ser usado em outro lugar
+export default FirstComponent
