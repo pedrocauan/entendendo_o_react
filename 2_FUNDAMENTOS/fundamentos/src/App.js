e6312ad99a33edaf1160f1e6b0baf76f2f6bf48d
pedrocauan <mainyasuo92@gmail.com>
2022-07-16 04:12:50 -0300

    importação do componente no app.js

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
+
+//components
+import FirstComponent from "./components/FirstComponent"
+
+//styles | css
 import './App.css';
 
+//Index.html -> fundamentos (página home)
 function App() {
   return (
     // ==== CODIGO AQUI  ====
     <div className="App">
-      <h1>Fundamentos do Reeact</h1>
+      <h1>Fundamentos do React</h1>
+      <FirstComponent></FirstComponent>
     </div>
   );
 }
 
 export default App;
+
