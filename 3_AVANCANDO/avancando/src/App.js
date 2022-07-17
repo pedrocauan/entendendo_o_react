16924bb6b788bec150bbe5f72b1a7bdacd71369d
pedrocauan <mainyasuo92@gmail.com>
2022-07-17 12:26:00 -0300

    colocando imagens que estao nap asta public

--- 3_AVANCANDO/avancando/src/App.js
+++ 3_AVANCANDO/avancando/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Avançando em react</h1>
+      <div>
+        {/* Imagem em public */}
+        <img src="/aaaacalculadora.png" alt="imagem de uma calculadora aesthetic com fundo rosa" />
+      </div>
     </div>
   );
 }
