da1b956fb5c81afbdf9addf13d235f16bcaf25f9
pedrocauan <mainyasuo92@gmail.com>
2022-07-16 23:27:40 -0300

    Conhecendo o JSX

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -2,9 +2,17 @@
 //Componente
 const FirstComponent = () => {
     //O que ele vai retornar
+
+    //O JSX  é o html do REACT, nele serão exibidas as tags HTML renderizadas no navegador
+    //alguns atributos do CSS podem gerar conflitos com o css e voce terá que trocar
+    //Atributos:
+
+    // class | className
     return (
         <div>
-            <h1>Primeiro componente</h1>
+            <h1>Titulo</h1>
+
+            <p className="teste">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, odio quae quasi officia quidem culpa natus voluptate id error tempora provident quis pariatur quia consectetur dolorem iste facilis inventore minus!</p>
         </div>
     )
 }
