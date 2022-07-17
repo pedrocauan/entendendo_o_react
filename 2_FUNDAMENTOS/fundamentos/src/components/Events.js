73f12d5723d510f1b3284092b5e2af3f39e006c9
pedrocauan <mainyasuo92@gmail.com>
2022-07-17 02:25:00 -0300

    funções nos eventos

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -16,6 +16,15 @@ const Events = () => {
                 {/*Caso use o handle como função ( handle() ) ele vai executar a função antes do evento acontecer*/}
                 <button onClick={handleMyEvent} >Clique aqui</button>
             </div>
+            <div>
+                {/* Eventos nas funções são usado quando tarefas simples são necessárias ou é preciso fazer mudanças*/}
+                <button onClick = { () => console.log("Tu clicou aqui através de uma função rodada no evento da pasta components/Events.js")}>Clique aqui também</button>
+                {/*Nestes casos, para facilitar a visualização do html é aconselhavel usar as hanEvents */}
+                <button onClick = { () => {
+                    if(true)
+                        console.log("isso não deveria existir porque é gambiarra =D")
+                }}>Clica aqui pfv</button>
+            </div>
         </div>
     )
 }
